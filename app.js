25f533d94ec5097224cd31f3ac80d676f9f40007
YU-CHE CHEN <job047587213@hotmail.com>
2020-08-23 01:50:58 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
-
 const restaurantLists = require('./models/restaurantList')
-
 const app = express()
 const port = 3000
-
+const session = require('express-session')
 const routes = require('./routes')
 require('./config/mongoose')
 
@@ -31,6 +29,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(routes)
+
+//setting express-session
+app.use(session({
+  secret: 'ThisiIsMySecret',
+  resave: false,
+  saveUninitialized: true
+}))
 
 
 //start and listen on the express server
